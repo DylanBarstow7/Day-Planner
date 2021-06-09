23003843db4cad8fc28a55082c22f5dd3b3b9605
Dylan Barstow <dcbarstow7@gmail.com>
2021-06-09 16:49:39 -0500

    connected style sheet to individual time-blocks

--- Assets/main.js
+++ Assets/main.js
@@ -14,3 +14,29 @@ function saveText() {
 document.querySelector("#currentDay").innerHTML =    
     moment().format("MMMM Do YYYY");
 /**formats date as Jun 9th, 2021 */
+
+updateHour();
+
+/**Running the updateHour function every 30 seconds */
+const timeCheck = setInterval(updateHour, 30000);
+
+/** depending on the current time, the past, present, or future id will be chosen and applied to the respected time-blocks */
+function updateHour() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+        let hour = parseInt($(this).attr('id'));
+        if (hour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        } else if (hour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
