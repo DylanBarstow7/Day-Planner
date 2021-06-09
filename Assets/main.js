f9a2f8cc8356114504661bb9c35820f38384bd4c
Dylan Barstow <dcbarstow7@gmail.com>
2021-06-09 16:28:07 -0500

    Connected JS file, added local storage, added momentjs

--- Assets/main.js
+++ Assets/main.js
@@ -1,8 +1,16 @@
 /**Creating Save button */
 document.querySelectorAll(".saveBtn").forEach((btn) => {
-    btn.addEventListener("click", saveTask);
+    btn.addEventListener("click", saveText);
 });
 
+/**Creating local storage */
+function saveText() {
+    let hour = $(this).parent().attr("id");
+    let task = $(this).siblings(".description").val();
+    localStorage.setItem(hour, task);
+}
 
-
-
+/**Adding Momentjs to the page*/
+document.querySelector("#currentDay").innerHTML =    
+    moment().format("MMMM Do YYYY");
+/**formats date as Jun 9th, 2021 */
